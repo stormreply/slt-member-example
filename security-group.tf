171820383ae9153e3370f2178dcac2e8160c9c3f
Bernd Herding <bernd.herding@external.biontech.com>
2025-07-10 17:55:38 +0200

    modify test security group description

--- security-group.tf
+++ security-group.tf
@@ -1,8 +1,4 @@
 resource "aws_security_group" "test" {
   # checkov:skip=CKV2_AWS_5: "Ensure that Security Groups are attached to another resource"
-  description = <<-EOD
-    This security group is empty on purpose. It us used to quickly
-    build a resource in order to test the Storm Library for Terraform
-    Github workflows.
-  EOD
+  description = "This a test resource; empty on purpose."
 }
