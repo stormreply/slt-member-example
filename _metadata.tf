ef51d973c32b1eb9a69c13a96c533a1b94a597eb
Bernd Herding <bernd.herding@external.biontech.com>
2025-10-15 22:45:30 +0200

    use _default_tags and _metadata

---
+++ _metadata.tf
@@ -0,0 +1,45 @@
+locals {
+  _metadata = merge(
+    var._metadata,
+    var._metadata.deployment == "" ? { deployment = basename(abspath(path.module)) } : {},
+    var._metadata.short_name == "" ? { short_name = basename(abspath(path.module)) } : {}
+  )
+}
+
+locals {
+  _deployment = local._metadata.deployment
+  _short_name = local._metadata.shortname
+}
+
+variable "_metadata" {
+  type = object({
+    actor       = string # Github actor (deployer) of the deployment
+    catalog_id  = string # SLT catalog id of this module
+    environment = string # environment of the deployment, defaults to <actor>
+    deployment  = string # slt-<catalod_id>-<repo>-<environment>
+    ref         = string # Git reference of the deployment
+    ref_name    = string # Git ref_name (branch) of the deployment
+    repo        = string # GitHub short repository name (without owner) of the deployment
+    repository  = string # GitHub full repository name (including owner) of the deployment
+    sha         = string # Git (full-length, 40 char) commit SHA of the deployment
+    short_name  = string # slt-<cat>-<environment>
+    time        = string # Timestamp of the deployment
+  })
+  default = {
+    actor       = ""
+    catalog_id  = ""
+    environment = ""
+    deployment  = ""
+    ref         = ""
+    ref_name    = ""
+    repo        = ""
+    repository  = ""
+    sha         = ""
+    short_name  = ""
+    time        = ""
+  }
+}
+
+output "_metadata" {
+  value = var._metadata
+}
