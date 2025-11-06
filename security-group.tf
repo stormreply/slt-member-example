3e84d210826c0fdb701af9d48270a82df5ae0b4f
Bernd Herding <bernd.herding@external.biontech.com>
2025-11-06 12:14:10 +0100

    use _name_tag

--- security-group.tf
+++ security-group.tf
@@ -1,8 +1,8 @@
 resource "aws_security_group" "test" {
   # checkov:skip=CKV2_AWS_5: "Ensure that Security Groups are attached to another resource"
   description = "This a test resource; empty on purpose."
-  name        = local._deployment
+  name        = local._name_tag
   tags = {
-    Name = local._deployment
+    Name = local._name_tag
   }
 }
