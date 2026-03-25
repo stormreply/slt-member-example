079e694b3dfa0d3e98fbedd14c0eb5acf5a02ec1
Bernd Herding <bernd.herding@external.biontech.com>
2026-03-25 21:02:32 +0100

    add summary output

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,10 @@ output "my_variable" {
   description = "The value of my_variable"
   value       = var.my_variable
 }
+
+output "_summary" {
+  description = "Key-value pairs to be published in the GITHUB_STEP_SUMMARY"
+  value = {
+    my_variable = var.my_variable
+  }
+}
