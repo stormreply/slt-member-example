2c9e59de41d75e82190f17bdd1e3b9de57b0e6ca
Bernd Herding <bernd.herding@external.biontech.com>
2026-04-16 17:33:37 +0200

    update from slt-repo-template

--- security-group.tf
+++ security-group.tf
@@ -1,5 +1,5 @@
 resource "aws_security_group" "test" {
   # checkov:skip=CKV2_AWS_5: "Ensure that Security Groups are attached to another resource"
   description = "This a test resource; empty on purpose."
-  name        = local._name_tag
+  name        = local._deployment
 }
